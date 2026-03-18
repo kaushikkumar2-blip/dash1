2bcc083c1949a602af324ecf1d3e195b9aff8e6b
kaushik kumar <kaushik.kumar2@flipkart.com>
2026-03-18 14:19:32 +0530

    Update seller_dashboard (1).py

--- seller_dashboard (1).py
+++ seller_dashboard (1).py
@@ -205,7 +205,7 @@ def load_client_map(path: str) -> dict:
     except FileNotFoundError:
         pass
     return mapping
-CLIENT_MAP = load_client_map("clients.xlsx")
+CLIENT_MAP = load_client_map("clients.csv")
 
 
 def _resolve_client(seller_str):
